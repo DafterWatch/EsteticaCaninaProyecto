021e489966c2cbe0f6c1faea7a849a2da7cbdde8
DafterWatch <marcosingefa@gmail.com>
2021-11-04 00:00:04 -0400

    registro clientes

--- ApiNodejs/rutas.js
+++ ApiNodejs/rutas.js
@@ -12,6 +12,18 @@ router.post('/getUsuariosLogin',(req, res)=>{
         }
     })
 });
+//agregar usuarios
+router.post('/addUsuario',(req, res)=>{
+    const {nombreUsuario, telefono, correo, cargo, contra} = req.body
+    let sql = `insert into usuarios(nombreUsuario, telefono, correo, cargo, contra) 
+                values('${nombreUsuario}','${telefono}','${correo}','${cargo}','${contra}')`
+    conexion.query(sql, (err, rows, fields)=>{
+        if(err) throw err
+        else {
+            res.json({status: 'usuario agregado'});
+        }
+    })
+});
 //--------------------
 
 module.exports = router;
--- EsteticaCanina/src/app/pages/registro/registro.component.ts
+++ EsteticaCanina/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosDetailsService, UsuarioRegistro } from 'src/app/services/usuarios-details.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router, private usuarioService:UsuariosDetailsService) { }
 
   ngOnInit(): void {
   }
-
+  reppass = '';
+  usuarioRegistro:UsuarioRegistro = {
+    nombreUsuario: '',
+    telefono: '',
+    correo: '',
+    cargo: 'cliente',
+    contra: ''
+  }
+  registrarNuevoUsuario(){    
+    if(this.usuarioRegistro.nombreUsuario.length>0 && this.usuarioRegistro.contra.length>0 && this.reppass.length>0 && this.usuarioRegistro.correo.length>0){
+      if(this.usuarioRegistro.contra == this.reppass){
+        this.usuarioService.addUsuarioRegistro(this.usuarioRegistro).subscribe();
+        alert("Se ha registrado un nuevo usuario");
+        this.router.navigate(['login']);
+      } else {
+        alert("Las contraseñas no coinciden");
+      }
+    } else {
+      alert("Porfavor complete los campos vacios");
+    }
+  }
 }
