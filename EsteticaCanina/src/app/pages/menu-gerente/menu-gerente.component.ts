64d4778e879259fd3117add99c2aff9936376fd1
DafterWatch <marcosingefa@gmail.com>
2021-11-04 01:51:03 -0400

    registro empleados +

--- EsteticaCanina/src/app/pages/menu-gerente/menu-gerente.component.ts
+++ EsteticaCanina/src/app/pages/menu-gerente/menu-gerente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosDetailsService, UsuarioRegistro } from 'src/app/services/usuarios-details.service';
 
 @Component({
   selector: 'app-menu-gerente',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuGerenteComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router, private usuarioService:UsuariosDetailsService) { }
 
   ngOnInit(): void {
   }
-
+  reppass = '';
+  usuarioRegistro:UsuarioRegistro = {
+    nombreUsuario: '',
+    telefono: '',
+    correo: '',
+    cargo: 'empleado',
+    contra: ''
+  }
+  registrarNuevoUsuario(){    
+    if(this.usuarioRegistro.nombreUsuario.length>0 && 
+      this.usuarioRegistro.contra.length>0 && 
+      this.reppass.length>0 && 
+      this.usuarioRegistro.correo.length>0 &&
+      this.usuarioRegistro.telefono.length>0){
+      if(this.usuarioRegistro.contra == this.reppass){
+        this.usuarioService.addUsuarioRegistro(this.usuarioRegistro).subscribe();
+        alert("Se ha registrado un nuevo empleado");
+      } else {
+        alert("Las contraseñas no coinciden");
+      }
+    } else {
+      alert("Porfavor complete los campos vacios");
+    }
+  }
 }
